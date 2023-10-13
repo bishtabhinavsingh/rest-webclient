3f718794bda0a3d61fb83c2177f25f2ff19f16f6
Abhinav Singh Bisht <bishtabhinavsingh@gmail.com>
2023-10-13 10:59:35 -0400

    Initial commit, async rest calls

---
+++ src/main/java/com/beasttech/restfulapp/Controller.java
@@ -0,0 +1,38 @@
+package com.beasttech.restfulapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    RestServiceCaller restServiceCaller;
+
+    @GetMapping("/apiget")
+    public String getEndPoint(@RequestParam("params") String params){
+        Map<String, String> methodsAndURLs = new HashMap<>();
+        methodsAndURLs.put("GET", "http://localhost:8090/async/gettest");
+        methodsAndURLs.put("PUT", "http://localhost:8090/async/puttest");
+        methodsAndURLs.put("POST", "http://localhost:8090/async/posttest");
+        methodsAndURLs.put("DELETE", "http://localhost:8090/async/deletetest");
+
+        try{
+            for (Map.Entry<String, String> entry : methodsAndURLs.entrySet()) {
+                ResponseEntity<String> response = restServiceCaller.requestAsync(params,HttpMethod.resolve(entry.getKey()),entry.getValue());
+                methodsAndURLs.put(entry.getKey(),response.getBody());
+                System.out.println(entry.getKey() + "  :  " +response.getBody());
+            }
+            return methodsAndURLs.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/beasttech/restfulapp/RestServiceCaller.java
@@ -0,0 +1,49 @@
+package com.beasttech.restfulapp;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class RestServiceCaller {
+    private final WebClient webClient = WebClient.create();
+
+    public ResponseEntity<String> requestAsync(Object message, HttpMethod method, String URL){
+        WebClient.RequestBodySpec request = prepareRequest(URL, method, message);
+        return request.retrieve()
+                .toEntity(String.class)
+                .block();
+    }
+
+    private WebClient.RequestBodySpec prepareRequest(String URL, HttpMethod method, Object message){
+        WebClient.RequestBodySpec request = null;
+        if (HttpMethod.GET.equals(method)){
+            request = (WebClient.RequestBodySpec) webClient.get().uri(messageToURI(message, URL));
+        } else if (HttpMethod.PUT.equals(method)){
+            request = (WebClient.RequestBodySpec) webClient.put().uri(URL).body(Mono.just(message), message.getClass());
+        } else if (HttpMethod.POST.equals(method)){
+            request = (WebClient.RequestBodySpec) webClient.post().uri(URL).body(Mono.just(message), message.getClass());
+        } else if (HttpMethod.DELETE.equals(method)){
+            request = (WebClient.RequestBodySpec) webClient.delete().uri(URL+"/"+message);
+    }
+        return request;
+    }
+
+    private String messageToURI(Object message, String URL){
+        if (message instanceof String) return URL+"?variable="+message;
+        if (message instanceof Map)
+            return ((Map<?, ?>) message).entrySet()
+                .stream()
+                    .map(x -> x.getKey()+"?"+x.getValue())
+                    .collect(Collectors.joining("&"));
+        return null;
+    }
+
+}
